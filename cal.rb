c7ad8f47b703a4dff2e71a9573c511cdaa8b40f5
いくこ <76399023+ikukotky@users.noreply.github.com>
2021-04-02 14:37:45 +0900

    Create cal.rb

---
+++ cal.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require "date"
+require "optparse"
+
+params = ARGV.getopts("", "y:#{Date.today.year}", "m:#{Date.today.month}")
+ 
+year = params.values[0].to_i
+month = params.values[1].to_i
+
+first_date = Date.new(year,month,1)
+last_date = Date.new(year,month,-1)
+last_day = last_day.day
+firstday_wday = first_day.wday
+
+hash {}
+1.upto(last_date) do |n|
+	puts n
+	hash[n] = (first_date + n - 1).wday
+end
+
+puts (#{month}月 #{year}).center(20)
+puts "日 月 火 水 木 金 土"
+print "  " * firstday_wday
+
+hash.each do |day, wday|
+	if wday % 7 == 0
+		print "\n"
+	end
+	print day.to_s.rjust(2) + " "
+end
+if last_date != 0
+	print "\n"
+end
