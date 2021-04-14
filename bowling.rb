c58aaada89b92cf21945da21d8c4709573159213
いくこ <76399023+ikukotky@users.noreply.github.com>
2021-04-14 17:06:12 +0900

    Create bowling.rb

---
+++ bowling.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+score = ARGV[0]
+scores = score.split(',')
+shots = []
+scores.each do |s|
+	if s == 'X'
+		shots << 10
+		shots << 0
+	else
+		shots << s.to_i
+	end
+end
+
+frames = Array.new(0)
+shots.each_slice(2) do |s|
+	frames << s
+end
+
+point = 0
+frames.each_with_index do |frame, i|
+	if frame[0] == 10
+		point += 10 + frames[i + 1].sum
+	elsif frames[i].sum == 10
+		point += 10 + frames[i + 1][0]
+	else
+		point += frame.sum
+	end
+end
+p point
